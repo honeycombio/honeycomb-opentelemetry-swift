6ae50578ff7d8fbda502f4a200e8a6e849c647a3
Bee Klimt <beeklimt@honeycomb.io>
2024-08-21 14:14:21 -0700

    add a comment to clarify a sleep

--- Tests/SmokeTests/HoneycombSmokeTests.swift
+++ Tests/SmokeTests/HoneycombSmokeTests.swift
@@ -12,6 +12,7 @@ final class HoneycombSmokeTests: XCTestCase {
             let options = try HoneycombOptions.Builder()
                 .setAPIKey("test-key")
                 .setApiEndpoint("http://localhost:4318")
+                .setServiceName("swift-test")
                 .setDebug(true)
                 .build()
             try Honeycomb.configure(options: options)
@@ -24,6 +25,11 @@ final class HoneycombSmokeTests: XCTestCase {
         let tracerProvider = OpenTelemetry.instance.tracerProvider as! TracerProviderSdk
         tracerProvider.forceFlush()
         tracerProvider.shutdown()
+        // tracerProvider.forceFlush() starts an async http operation, and holds only a weak
+        // reference to itself. So, if the test quits immediately, the whole thing will be
+        // garbage-collected and the http request will never be sent. Until that behavior is
+        // fixed, it's necessary to sleep here, to allow the outstanding HTTP requests to be
+        // processed.
         Thread.sleep(forTimeInterval: 5.0)
     }
 
@@ -35,4 +41,3 @@ final class HoneycombSmokeTests: XCTestCase {
         span.end()
     }
 }
- 
