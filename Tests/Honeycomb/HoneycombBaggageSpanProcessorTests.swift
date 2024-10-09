30f20661cb5ac9e96ba101e7e60454802176f1af
Martin Holman <me@martinholman.co.nz>
2024-10-09 10:59:06 -0700

    Add test for propagation

--- Tests/Honeycomb/HoneycombBaggageSpanProcessorTests.swift
+++ Tests/Honeycomb/HoneycombBaggageSpanProcessorTests.swift
@@ -1,3 +1,4 @@
+import InMemoryExporter
 import OpenTelemetryApi
 import OpenTelemetrySdk
 import XCTest
@@ -15,5 +16,51 @@ class HoneycombBaggageSpanProcessorTests: XCTestCase {
         XCTAssertFalse(processor.isEndRequired)
         processor.forceFlush()
         processor.shutdown()
+    }
+
+    func testPropagation() {
+        let processor = HoneycombBaggageSpanProcessor(filter: { (e) in return true })
+        let exporter = InMemoryExporter()
+        let simple = SimpleSpanProcessor(spanExporter: exporter)
+        OpenTelemetry.registerTracerProvider(
+            tracerProvider: TracerProviderBuilder().add(spanProcessor: processor)
+                .add(spanProcessor: simple).build()
+        )
+        let tracer = OpenTelemetry.instance.tracerProvider.get(
+            instrumentationName: "test",
+            instrumentationVersion: "1.0.0"
+        )
+
+        if let key = EntryKey(name: "test-key") {
+            if let value = EntryValue(string: "test-value") {
+                let parent = tracer.spanBuilder(spanName: "parent").startSpan()
+                let b = OpenTelemetry.instance.baggageManager.baggageBuilder()
+                    .put(key: key, value: value, metadata: nil).build()
+                OpenTelemetry.instance.contextProvider.setActiveBaggage(b)
+
+                let child = tracer.spanBuilder(spanName: "child").startSpan()
+
+                child.end()
+                parent.end()
+            }
+        }
+
+        simple.forceFlush()
+
+        let spans = exporter.getFinishedSpanItems()
+        XCTAssert(spans.count == 2)
+
+        let child = spans.first(where: { s in return s.name == "child" })
+        XCTAssert(child != nil)
+
+        let parent = spans.first(where: { s in return s.name == "parent" })
+        XCTAssert(parent != nil)
+
+        XCTAssert(child?.attributes.count == 1)
+
+        let attr = child?.attributes.first
+
+        XCTAssert(attr?.key == "test-key")
+        XCTAssert(attr?.value.description == "test-value")
     }
 }
