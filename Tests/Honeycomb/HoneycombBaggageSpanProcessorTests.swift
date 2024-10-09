7cdd7bc17c05ff52cdd709d0981ce11da4b5f70f
Martin Holman <me@martinholman.co.nz>
2024-10-08 17:21:37 -0700

    Add basic test for baggage span processor

---
+++ Tests/Honeycomb/HoneycombBaggageSpanProcessorTests.swift
@@ -0,0 +1,19 @@
+import XCTest
+import OpenTelemetryApi
+import OpenTelemetrySdk
+
+@testable import Honeycomb
+
+class HoneycombBaggageSpanProcessorTests: XCTestCase {
+    let readableSpan = ReadableSpanMock()
+    
+    func testNoCrash() {
+        let processor = HoneycombBaggageSpanProcessor(filter: { (e) in return true })
+        processor.onStart(parentContext: nil, span: readableSpan)
+        XCTAssertTrue(processor.isStartRequired)
+        processor.onEnd(span: readableSpan)
+        XCTAssertFalse(processor.isEndRequired)
+        processor.forceFlush()
+        processor.shutdown()
+    }
+}
