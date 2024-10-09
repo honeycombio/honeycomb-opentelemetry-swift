0a5fa61c7df3f70b745d1952a38179253390ee32
Martin Holman <me@martinholman.co.nz>
2024-10-08 17:20:33 -0700

    Add basic baggage span processor

---
+++ Sources/Honeycomb/HoneycombBaggageSpanProcessor.swift
@@ -0,0 +1,24 @@
+import Foundation
+import OpenTelemetrySdk
+import OpenTelemetryApi
+
+public struct HoneycombBaggageSpanProcessor: SpanProcessor {
+    public let isStartRequired = true
+    public let isEndRequired = false
+    public let filter: (Entry) -> Bool
+        
+    public func onStart(parentContext: OpenTelemetryApi.SpanContext?, span: any OpenTelemetrySdk.ReadableSpan) {
+        if let baggage = OpenTelemetry.instance.contextProvider.activeBaggage {
+            let filteredEntries = baggage.getEntries().filter(self.filter)
+            for entry in filteredEntries {
+                span.setAttribute(key: entry.key.name, value: entry.value.string)
+            }
+        }
+    }
+    
+    public func onEnd(span: any OpenTelemetrySdk.ReadableSpan) {}
+    
+    public func shutdown(explicitTimeout: TimeInterval? = nil) {}
+
+    public func forceFlush(timeout: TimeInterval? = nil) {}
+}
