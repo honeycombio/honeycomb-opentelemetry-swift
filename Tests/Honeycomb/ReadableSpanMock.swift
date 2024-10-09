25ac35a26af2c44138d21c116f5b921d85648796
Martin Holman <me@martinholman.co.nz>
2024-10-09 12:22:08 -0700

    :lipstick: appease the linter

--- Tests/Honeycomb/ReadableSpanMock.swift
+++ Tests/Honeycomb/ReadableSpanMock.swift
@@ -1,7 +1,5 @@
-/*
- * Copyright The OpenTelemetry Authors
- * SPDX-License-Identifier: Apache-2.0
- */
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
 
 import Foundation
 import OpenTelemetryApi
