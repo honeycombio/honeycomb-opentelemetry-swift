691f732d9e4761f32ff9fa109082eb785293d59c
Arriana Blais <arriblais@honeycomb.io>
2024-10-18 11:26:58 -0400

    print to match otel swift

--- Sources/Honeycomb/HoneycombDebug.swift
+++ Sources/Honeycomb/HoneycombDebug.swift
@@ -1,30 +1,14 @@
-import os;
+import Foundation;
 
 internal func debugOptions(options: HoneycombOptions) {
-
-    if !options.debug {
-        return;
-    }
     
-    if #available(OSX 11.0, *) {
-        let sdkLog = Logger(subsystem: "io.honeycomb", category: "SDK");
 
-        sdkLog.debug("Honeycomb SDK Debug Mode Enabled")
+    if options.debug {
+        print("Honeycomb SDK Debug Mode Enabled")
 
-        sdkLog.debug("API Key configured for traces: \(options.tracesApiKey)")
-        sdkLog.debug("Service Name configured for traces: \(options.serviceName)")
-        sdkLog.debug("Endpoint configured for traces: \(options.tracesEndpoint)")
-        sdkLog.debug("Sample Rate configured for traces: \(options.sampleRate)")
-    } else {
-        let sdkLog = OSLog(subsystem: "io.honeycomb", category: "SDK")
-
-        os_log("Honeycomb SDK Debug Mode Enabled", log: sdkLog, type: .debug)
-
-        // API Key
-        os_log("API Key configured for traces: %@", log: sdkLog, type: .debug, options.tracesApiKey)
-        os_log("Service Name configured for traces: %@", log: sdkLog, type: .debug, options.serviceName)
-        os_log("Endpoint configured for traces: %@", log: sdkLog, type: .debug, options.tracesEndpoint)
-        os_log("Sample Rate configured for traces: %@", log: sdkLog, type: .debug, options.sampleRate)
+        print("API Key configured for traces: \(options.tracesApiKey)")
+        print("Service Name configured for traces: \(options.serviceName)")
+        print("Endpoint configured for traces: \(options.tracesEndpoint)")
+        print("Sample Rate configured for traces: \(options.sampleRate)")
     }
-
 }
