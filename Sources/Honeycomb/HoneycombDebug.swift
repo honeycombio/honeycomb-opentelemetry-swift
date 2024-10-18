626cc84a8ee2c9c09e8690d7274cc067d0d42275
Arriana Blais <arriblais@honeycomb.io>
2024-10-18 10:19:28 -0400

    debug SDK options

---
+++ Sources/Honeycomb/HoneycombDebug.swift
@@ -0,0 +1,30 @@
+import os;
+
+public func debugOptions(options: HoneycombOptions) {
+
+    if !options.debug {
+        return;
+    }
+    
+    if #available(OSX 11.0, *) {
+        let sdkLog = Logger(subsystem: "io.honeycomb", category: "SDK");
+
+        sdkLog.debug("Honeycomb SDK Debug Mode Enabled")
+
+        sdkLog.debug("API Key configured for traces: \(options.tracesApiKey)")
+        sdkLog.debug("Service Name configured for traces: \(options.serviceName)")
+        sdkLog.debug("Endpoint configured for traces: \(options.tracesEndpoint)")
+        sdkLog.debug("Sample Rate configured for traces: \(options.sampleRate)")
+    } else {
+        let sdkLog = OSLog(subsystem: "io.honeycomb", category: "SDK")
+
+        os_log("Honeycomb SDK Debug Mode Enabled", log: sdkLog, type: .debug)
+
+        // API Key
+        os_log("API Key configured for traces: %@", log: sdkLog, type: .debug, options.tracesApiKey)
+        os_log("Service Name configured for traces: %@", log: sdkLog, type: .debug, options.serviceName)
+        os_log("Endpoint configured for traces: %@", log: sdkLog, type: .debug, options.tracesEndpoint)
+        os_log("Sample Rate configured for traces: %@", log: sdkLog, type: .debug, options.sampleRate)
+    }
+
+}
