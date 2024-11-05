437e29cf03c509c675634fe88982152525fefff3
Bee Klimt <beeklimt@honeycomb.io>
2024-11-05 11:35:31 -0800

    revert run-ios-tests.sh

--- run-ios-tests.sh
+++ run-ios-tests.sh
@@ -1,6 +1,6 @@
 set -e
 
-SDK="iphonesimulator18.0"
+SDK="iphonesimulator17.5"
 DESTINATION="OS=17.5,name=iPhone 15"
 
 xcodebuild test -scheme HoneycombTests -sdk "$SDK" -destination "$DESTINATION"
