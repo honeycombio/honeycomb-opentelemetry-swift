3b3a8a00203f1495fac0c9ddd9cacd15889e610e
Wolfgang Therrien <wolfgangtherrien@honeycomb.io>
2025-01-24 11:44:29 -0500

    Remove uneeded member.

--- Sources/Honeycomb/Session/SessionStorage.swift
+++ Sources/Honeycomb/Session/SessionStorage.swift
@@ -1,14 +1,13 @@
 import Foundation
 
-internal let sessionIdKey: String = "session.id"
-internal let sessionStartTimeKey: String = "session.startTime"
+private let sessionIdKey: String = "session.id"
+private let sessionStartTimeKey: String = "session.startTime"
 
+// A set of utility functions for reading and saving a Session object to persistent storage
 struct SessionStorage {
-    let userDefaults = UserDefaults.standard 
-
     func read() -> Session? {
-        guard let id = userDefaults.string(forKey: sessionIdKey),
-            let startTimestamp = userDefaults.object(forKey: sessionStartTimeKey)
+        guard let id = UserDefaults.standard.string(forKey: sessionIdKey),
+            let startTimestamp = UserDefaults.standard.object(forKey: sessionStartTimeKey)
                 as? Date
         else {
             // If the saves session is garbo, return sentienel value to indicate there's no existing session
@@ -19,16 +18,15 @@ struct SessionStorage {
     }
 
     func save(session: Session) {
-        userDefaults.set(session.id, forKey: sessionIdKey)
-        userDefaults.set(session.startTimestamp, forKey: sessionStartTimeKey)
+        UserDefaults.standard.set(session.id, forKey: sessionIdKey)
+        UserDefaults.standard.set(session.startTimestamp, forKey: sessionStartTimeKey)
     }
 
     func clear() {
-        userDefaults.set("", forKey: sessionIdKey)
-        userDefaults.set(
+        UserDefaults.standard.set("", forKey: sessionIdKey)
+        UserDefaults.standard.set(
             Date.distantPast,
             forKey: sessionStartTimeKey
         )
-
     }
 }
