6f8fb5cb6257e6673bd6128bf05399fb6f4eb95c
Bee Klimt <beeklimt@honeycomb.io>
2025-02-26 13:23:43 -0800

    feat: add api to enable or disable instrumentation

--- Tests/Honeycomb/HoneycombOptionsTests.swift
+++ Tests/Honeycomb/HoneycombOptionsTests.swift
@@ -61,6 +61,12 @@ final class HoneycombOptionsTests: XCTestCase {
         XCTAssertEqual(OTLPProtocol.httpProtobuf, options.tracesProtocol)
         XCTAssertEqual(OTLPProtocol.httpProtobuf, options.metricsProtocol)
         XCTAssertEqual(OTLPProtocol.httpProtobuf, options.logsProtocol)
+
+        XCTAssertTrue(options.metricKitInstrumentationEnabled)
+        XCTAssertTrue(options.urlSessionInstrumentationEnabled)
+        XCTAssertTrue(options.uiKitInstrumentationEnabled)
+        XCTAssertFalse(options.touchInstrumentationEnabled)
+        XCTAssertTrue(options.unhandledExceptionInstrumentationEnabled)
     }
 
     func testOptionsWithEmptyStrings() throws {
@@ -331,6 +337,11 @@ final class HoneycombOptionsTests: XCTestCase {
             .setTracesProtocol(OTLPProtocol.grpc)
             .setMetricsProtocol(OTLPProtocol.grpc)
             .setLogsProtocol(OTLPProtocol.grpc)
+            .setMetricKitInstrumentationEnabled(false)
+            .setURLSessionInstrumentationEnabled(false)
+            .setUIKitInstrumentationEnabled(false)
+            .setTouchInstrumentationEnabled(true)
+            .setUnhandledExceptionInstrumentationEnabled(false)
             .build()
 
         XCTAssertEqual("service", options.serviceName)
@@ -388,6 +399,12 @@ final class HoneycombOptionsTests: XCTestCase {
 
         XCTAssertTrue(options.debug)
         XCTAssertEqual(42, options.sampleRate)
+
+        XCTAssertFalse(options.metricKitInstrumentationEnabled)
+        XCTAssertFalse(options.urlSessionInstrumentationEnabled)
+        XCTAssertFalse(options.uiKitInstrumentationEnabled)
+        XCTAssertTrue(options.touchInstrumentationEnabled)
+        XCTAssertFalse(options.unhandledExceptionInstrumentationEnabled)
     }
 
     func testDatasetSetWithClassicKey() throws {
