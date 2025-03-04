f15fbce9419e392c52e97dddb9829178a54211f9
Arriana Blais <arriblais@honeycomb.io>
2025-03-04 14:29:17 -0500

    fix formatting

--- Sources/Honeycomb/HoneycombUncaughtExceptions.swift
+++ Sources/Honeycomb/HoneycombUncaughtExceptions.swift
@@ -2,13 +2,14 @@ import Foundation
 
 internal class HoneycombUncaughtExceptionHandler {
     private static var initialUncaughtExceptionHandler: ((NSException) -> Void)? = nil
-    
+
     public static func initializeUnhandledExceptionInstrumentation() {
-        HoneycombUncaughtExceptionHandler.initialUncaughtExceptionHandler = NSGetUncaughtExceptionHandler()
-        
+        HoneycombUncaughtExceptionHandler.initialUncaughtExceptionHandler =
+            NSGetUncaughtExceptionHandler()
+
         NSSetUncaughtExceptionHandler { exception in
             Honeycomb.log(exception: exception, thread: Thread.current)
-            
+
             if HoneycombUncaughtExceptionHandler.initialUncaughtExceptionHandler != nil {
                 HoneycombUncaughtExceptionHandler.initialUncaughtExceptionHandler!(exception)
             }
