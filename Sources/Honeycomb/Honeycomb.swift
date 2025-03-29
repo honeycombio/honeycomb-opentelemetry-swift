2de357b98f8e861a686a1448971d97030f348422
Martin Holman <me@martinholman.co.nz>
2025-03-28 17:04:50 -0700

    💅 format

--- Sources/Honeycomb/Honeycomb.swift
+++ Sources/Honeycomb/Honeycomb.swift
@@ -127,14 +127,14 @@ public class Honeycomb {
             )
 
         #if os(iOS) && !targetEnvironment(macCatalyst)
-        do {
-            let networkMonitor = try NetworkMonitor()
-            tracerProviderBuilder =
-                tracerProviderBuilder
-                .add(spanProcessor: NetworkStatusSpanProcessor(monitor: networkMonitor))
-        } catch {
-            NSLog("Unable to create NetworkMonitor: \(error)")
-        }
+            do {
+                let networkMonitor = try NetworkMonitor()
+                tracerProviderBuilder =
+                    tracerProviderBuilder
+                    .add(spanProcessor: NetworkStatusSpanProcessor(monitor: networkMonitor))
+            } catch {
+                NSLog("Unable to create NetworkMonitor: \(error)")
+            }
         #endif
 
         let tracerProvider =
