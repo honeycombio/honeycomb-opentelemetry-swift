e240446e234cb90244f2eb1e8def73a96a28729f
Bee Klimt <beeklimt@honeycomb.io>
2025-07-11 13:09:31 -0700

    tweak circleci settings

--- Examples/CocoaPodsTest/run-cocoapods-test.sh
+++ Examples/CocoaPodsTest/run-cocoapods-test.sh
@@ -1,12 +1,14 @@
 set -e
 
-# pod lib lint honeycomb-opentelemetry-swift.podspec
+pod lib lint --allow-warnings ../../honeycomb-opentelemetry-swift.podspec
 
+xcodebuild -showsdks | grep iphonesimulator
 SDK=$(xcodebuild -showsdks | grep iphonesimulator | sed -e 's/^.*-sdk //')
 echo "SDK: $SDK"
 
 DESTINATION="OS=18.2,name=iPhone 16"
 
 pod install --repo-update
+xcodebuild test -workspace CocoaPodsTest.xcworkspace -scheme CocoaPodsTest -sdk "$SDK" -showdestinations
 xcodebuild test -workspace CocoaPodsTest.xcworkspace -scheme CocoaPodsTest -sdk "$SDK" -destination "$DESTINATION" -verbose
 
